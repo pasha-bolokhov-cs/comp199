1ee8cf1c4bb6b8fce96b2ed8cfb81578aaf9af68
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-04-22 11:50:38 -0700

    Start adding tables

--- create-tables.sql
+++ create-tables.sql
@@ -1,1 +1,34 @@
-/* This is the script which creates initial database tables */
+/* 
+ * This is the script which creates initial database tables 
+ *
+ */
+
+/*
+ * Customers
+ */
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers (
+	customerId	int NOT NULL AUTO_INCREMENT,
+	name		varchar(80),
+	birth		date,
+	nationality	varchar(20),
+	passportNo	varchar(20),
+	passportExp	date,
+	email		varchar(255),
+	phone		varchar(80),
+	PRIMARY KEY	(customerId)
+);
+
+/*
+ * Cart
+ */
+DROP TABLE IF EXISTS cart;
+CREATE TABLE cart (
+	tripNo		int NOT NULL,
+	customerId	int,
+	startDate	date,
+	PRIMARY KEY	(tripNo),
+	FOREIGN KEY	(customerId) REFERENCES customers(customerId)
+			ON DELETE CASCADE ON UPDATE CASCADE
+);
+
