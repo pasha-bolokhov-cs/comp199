a1bc696d71d71d7c21b271ebfe7577df69083317
Candise Wang <lelaywang@gmail.com>
2015-04-24 11:58:23 -0700

    add example data to locations table

---
+++ locations.sql
@@ -0,0 +1,17 @@
+/*
+*
+*/
+
+
+insert into locations (locationId, region, country, city)
+values ('shanghai', 'Asia', 'China', 'Shanghai'),
+       ('tokyo', 'Asia', 'Japan', 'Tokyo'),
+	('sochi', 'Europe', 'Russia', 'Sochi'),
+	('paris', 'Europe', 'France','Paris');
+
+
+
+
+
+
+
