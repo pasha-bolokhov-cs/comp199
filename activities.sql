37140c870827b11f6bc329956d9e5dd440fdc1cb
Candise Wang <lelaywang@gmail.com>
2015-04-24 12:37:03 -0700

    Add surfing to activities

--- activities.sql
+++ activities.sql
@@ -5,5 +5,6 @@
 INSERT INTO activities (activityId, name, rate)
        VALUES ('fireworks', 'Fireworks', NULL),
 	      ('climbing', 'Climbing', NULL),
+	      ('surfing', 'Surfing', NULL),
               ('hunt_sharks','Hunting Sharks','NULL');
 
