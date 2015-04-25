ae6a1c2fcf8c9228e9b50098610380ff507e522e
Toshiyasu Akazawa <toshiyasu.akazawa@gmail.com>
2015-04-25 12:00:34 -0700

    Add some cities in Japan, Canada, and US

--- locations.sql
+++ locations.sql
@@ -6,9 +6,13 @@
 insert into locations (locationId, region, country, city)
 values ('shanghai', 'Asia', 'China', 'Shanghai'),
        ('tokyo', 'Asia', 'Japan', 'Tokyo'),
+       ('osaka','Asia','Japan','Osaka'),
+       ('hakata','Asia','Japan','Hakata'),
 	('sochi', 'Europe', 'Russia', 'Sochi'),
 	('paris', 'Europe', 'France','Paris'),
-        ('victoria','North America','Canada','Victoria');
+        ('victoria','North America','Canada','Victoria')
+        ('vancouver','North America','Canada','Vancouver')
+        ('seattle','North America','US','Seattle');
 
 
 
