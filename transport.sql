648823760c9a6db75cdd818dd73def894d723e0e
Toshiyasu Akazawa <toshiyasu.akazawa@gmail.com>
2015-04-26 21:54:51 -0700

    syntax fix and file name change from 'transoport.sql' to  'transport.sql'

---
+++ transport.sql
@@ -0,0 +1,17 @@
+/*
+* insert data into transport table
+*/
+INSERT INTO transport (transportId, type)
+       VALUE ('bus', 'Bus'),
+             ('vehicle_e', 'Economy'),
+	     ('vehicle_c', 'Compact'),
+	     ('vehicle_m', 'Minivan'),
+	     ('vehicle_v', 'Van'),
+       	     ('vechicle_s', 'Standard'),
+   	     ('vechicle_f', 'Full size'),
+	     ('vechicle_l', 'Luxury'),
+	     ('vechicle_su', 'SUV'),
+	     ('bike', 'Bike'),
+  	     ('train', 'Train'),
+	     ('ferry', 'Ferry'),
+             ('flight', 'Flight');
