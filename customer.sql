f32336087ba5e1973ef99adf596975752b45c9b2
Candise Wang <lelaywang@gmail.com>
2015-04-26 17:12:15 -0700

    Add one customer information to customer table
    Add "Paul Walker"

---
+++ customer.sql
@@ -0,0 +1,7 @@
+/*
+* Customer Account Information
+*/
+
+insert into customers (customerId,name,birth,nationality,passportNo,passportExp,email,phone)
+	value (1,'Paul Walker',todate('1973-09-12'),America,C01234567,todate('2017-06-30'),PWalker@gmail.com,1250349876);
+
