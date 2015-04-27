9cdc4eb13af176b63fd3df1299e7f498a2f8d0eb
Candise Wang <lelaywang@gmail.com>
2015-04-26 17:21:28 -0700

    Just fix the syntax

--- customer.sql
+++ customer.sql
@@ -3,5 +3,5 @@
 */
 
 insert into customers (customerId,name,birth,nationality,passportNo,passportExp,email,phone)
-	value (1,'Paul Walker',todate('1973-09-12'),America,C01234567,todate('2017-06-30'),PWalker@gmail.com,1250349876);
+	value (1,'Paul Walker',todate('1973-09-12'),'America','C01234567',todate('2017-06-30'),'PWalker@gmail.com','1250349876');
 
