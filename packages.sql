bf36d7f8724be51b1c3d859c5aac83f5a507adfd
Lelay-Wang <lelaywang@gmail.com>
2015-04-27 07:07:13 -0700

    Create packages.sql

---
+++ packages.sql
@@ -0,0 +1,8 @@
+/*
+* insert data into packages tables
+*/
+
+
+insert into packages (packageId, tripNo, origin, price, imageId, description, capacity, available,   )
+values (1, 1, 'Shanghai', 6000, 'p1', 'tobeupdated', 40, 40)
+        ;
