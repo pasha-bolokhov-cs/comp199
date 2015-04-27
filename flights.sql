fbef660ec3a6b0a293a92ca03ea8c12fac0d0de9
Toshiyasu Akazawa <toshiyasu.akazawa@gmail.com>
2015-04-26 22:11:18 -0700

    bug fix

--- flights.sql
+++ flights.sql
@@ -2,7 +2,7 @@
 *  insert data into flights table
 */
 
-INSERT INTO flights(flightId, alliance, departDate, arriveDate)
+INSERT INTO flights(alliance, departDate, arriveDate)
        VALUE ('AC003', '05/01/15 13:40', '05/02/15 15:20'),
 	     ('JL017', '05/01/15 14:15', '05/02/15 16:30'),
 	     ('NH115', '05/01/15 16:20', '05/02/15 18:30');
