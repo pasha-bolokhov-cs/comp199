ec59e1cbaac50f0c51500ce07b298f7a7d9c9a34
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-04-28 16:35:37 -0700

    Correct code style in 'segments.sql'

--- segments.sql
+++ segments.sql
@@ -6,7 +6,7 @@
 
 /* *** why is this using "activities" table? *** */
 INSERT INTO activities (segId, tripNo, transportId, flightId, locationId, hotelId, activityId, duration, dealPrice, nextSeg)
-       VALUES (1, 1, 'flight', null,'osaka', 'Hilton', 'climbing', 4, 6000, 2),
-              (2, 2, 'flight', null,'tokyo', 'Hilton', 'fireworks', 2, null, null),
-              (3, 3, 'flight', null,'osaka', 'Hilton', 'climbing', 4, 6000, 4),
-              (4, 4, 'flight', null,'hakata', 'Hilton', 'fireworks', 3, null, null);
+       VALUES (1, 1, 'flight', null, 'osaka', 'Hilton', 'climbing', 4, 6000, 2),
+              (2, 2, 'flight', null, 'tokyo', 'Hilton', 'fireworks', 2, null, null),
+              (3, 3, 'flight', null, 'osaka', 'Hilton', 'climbing', 4, 6000, 4),
+              (4, 4, 'flight', null, 'hakata', 'Hilton', 'fireworks', 3, null, null);
