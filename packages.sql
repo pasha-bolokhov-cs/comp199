956198a38a735bfb16b5a6e171dbd239a8679f97
Toshiyasu Akazawa <toshiyasu.akazawa@gmail.com>
2015-04-28 22:29:36 -0700

    add sample data 'Japan sight seeing'

--- packages.sql
+++ packages.sql
@@ -2,5 +2,6 @@
  * Data for 'packages' table
  */
 
-INSERT INTO packages (packageId, tripNo, origin, price, imageId, description, capacity, available)
-       VALUES (1, 1, 'Shanghai', 6000, 'p1', 'tobeupdated', 40, 40);
+INSERT INTO packages ( tripNo, origin, price, imageId, description, capacity, available)
+       VALUES ( 1, 'Shanghai', 6000, 'p1', 'tobeupdated', 40, 40),
+              ( 5, 'vancouver', 7000, null, 'Japan sight seeing', 40, 40);
