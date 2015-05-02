8a6a7e174b823bc3494284308af227af97844c93
Toshiyasu Akazawa <toshiyasu.akazawa@gmail.com>
2015-05-01 23:12:02 -0700

    bug fix -> flights , add new package and segments -> package.sql

--- sql/packages.sql
+++ sql/packages.sql
@@ -16,3 +16,14 @@ INSERT INTO segments (location, transportId, flightId, hotelId, activityId, dura
 INSERT INTO packages (segId, name, origin, price, description, capacity, available, imageName)
        VALUES (LAST_INSERT_ID(), 'Hawaii - Summer 2015', 'San Francisco', 700, 'Breath-taking vacation on Hawaii', 40, 40, NULL);
 
+/*
+ * Japan
+ */
+INSERT INTO segments (location, transportId, flightId, hotelId, activityId, duration, nextSeg)
+       VALUES ('Tokyo', 'flight', 'All Nippon Airlines - Summer 2015', NULL, NULL, 1, NULL );
+INSERT INTO segments (location, transportId, flightId, hotelId, activityId, duration, nextSeg)
+       VALUES ('Kyoto', 'train', NULL, 'Hilton', 'sightseeing', 6, LAST_INSERT_ID());
+INSERT INTO segments (location, transportId, flightId, hotelId, activityId, duration, nextSeg)
+       VALUES ('Vancouver', 'flight', 'Air Canada -Summer 2015', NULL, NULL, 1, LAST_INSERT_ID());
+INSERT INTO packages (segId, name, origin, price, description, capacity, available, imageName)
+       VALUES (LAST_INSERT_ID(), 'Kyoto - Summer 2015', 'Vancouver', 3500, 'Trip to traditional Japan', 40, 40, NULL);
