3f19d0f5b67f1b6d2690013a51191111e0b7f290
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-05-10 08:33:26 -0700

    Move 'refresh.sh' to the root of the web application

--- refresh.sh
+++ refresh.sh
@@ -9,18 +9,21 @@
 #     * the script assumes the existence of "travel" script which launches "mysql",
 #       and its availability in the path
 #
-#     * the script must only be run from the "sql" directory
+#     * the script must only be run from the root of the web application
 #
 
 AGENT=travel
+ERROR_MSG="This script must be run from the root of the web application"
 
-CURRDIR=$(pwd)
-CURRDIR=$(basename ${CURRDIR})
-
-if [ "x${CURRDIR}" != "xsql" ]; then
-	echo 'This script must be run from "sql" directory of the web program' 1>&2
+if [ ! -d "sql" ]; then
+	echo "${ERROR_MSG}" 1>&2
 	exit 1
 fi
+
+cd sql || {
+	echo "${ERROR_MSG}" 1>&2
+	exit 1
+}
 
 ${AGENT} <<EOF
 	source start.sql;
