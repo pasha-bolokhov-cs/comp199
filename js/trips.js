a5fffd4885e574abfe6c3e1c860b15cb27140372
Lelay-Wang <lelaywang@gmail.com>
2015-05-25 10:28:42 -0700

    add remove fuction

--- js/trips.js
+++ js/trips.js
@@ -29,5 +29,35 @@ app.controller('TripsController', function($scope, $rootScope, $http, $statePara
 	.finally(function() { 
 		$rootScope.waiting = false;
 	});
+	
+	/* remove the orders */
+	$scope.removeOrders = function() {
+		$rootScope.waiting = true;
+		$scope.orders = {};
+		$scope.orders.email = $rootScope.storage.token.email;
+		$scope.orders.package = $stateParams.package;
+		
+		$http.post("php/secure/remove-orders.php", $scope.orders)
+		.success(function(data) {
+			// process the response
+			if (data["error"]) {
+				if (data["error"] == "authentication")
+					$rootScope.doSignOut();
+				else
+					$scope.error = "Error: " + data["error"];
+			} else {
+				$scope.trips = data["data"];
+			}
+		})
+		.error(function(data, status) {
+			console.log(data);
+			$rootScope.error = "Error accessing the server: " + status + ".";
+		})
+		.finally(function() { 
+			$rootScope.waiting = false;
+		});
+				
+	}
+	$scope.removeOrders();
 
 });
