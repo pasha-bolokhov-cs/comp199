d72e75cb84d4babc0da4bf388ad86b62ea728452
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-05-25 03:44:06 -0700

    Correct a misspelling in "home.js"

--- js/home.js
+++ js/home.js
@@ -6,12 +6,12 @@ app.controller('HomeController', function($scope, $state) {
 	$scope.interval = 4000;
 
 	$scope.slides = [
+		{ image: "images/flip-flops-2.jpg", text: "Dreaming of a different you?",
+		  slogan: "Pour a handful of sand on your desk", button: "Start your change" },
 		{ image: "images/woman.jpg", text: "Office Work Brings you Nothing but Stress?",
 		  slogan: "Dream yourself out of the office cube", button: "Get out now" },
 		{ image: "images/traffic-2.jpg", text: "Commuting Feels Like There is No More Air?",
 		  slogan: "Get all the air of the world", button: "Start breathing" },
-		{ image: "images/flip-flops-2.jpg", text: "Dreaming of a different you?",
-		  slogan: "Pour a handful of sand on your desk", button: "Star your change" },
 		{ image: "images/starfish-2.jpg", text: "Stars are Calling upon you?",
 		  slogan: "Nothing is too far to reach", button: "Dive now" },
 		{ image: "images/xl_GreeceSunset.jpg", text: "Let the Horizon Come to You",
