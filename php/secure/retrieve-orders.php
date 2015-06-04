85fc5a62315442ebb4ba3a08dbdbd70ef28e6aae
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-06-03 23:12:41 -0700

    Convert "retrieve-orders.php" to use PDO

--- php/secure/retrieve-orders.php
+++ php/secure/retrieve-orders.php
@@ -15,6 +15,7 @@ $jsonData = file_get_contents("php://input");
 $data = json_decode($jsonData);
 
 /* validate data */
+$response = array();
 if (!array_key_exists("email", $token)) {
 	$response["error"] = "email-required";
 	goto quit;
@@ -33,115 +34,106 @@ define("MAX_RESPONSE_LINES", 1000);
 
 /* connect to the database */
 require_once '../../../../comp199-www/mysqli_auth.php';
-$mysqli = @new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
-if ($mysqli->connect_error) {
-	$response["error"] = 'Connect Error (' . $mysqli->connect_errno . ') ' .
-			     $mysqli->connect_error;
+try {
+	$dbh = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB, MYSQL_USER, MYSQL_PASS);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	$response["error"] = 'Connect Error: ' . $e->getMessage();
 	goto quit;
 }
 
 /* get customerId */
-if (!($customerId = get_customerId($mysqli, $token))) {
+if (!($customerId = get_customerId_PDO($dbh, $token))) {
 	goto auth_error_database;
 }
 
 /*
  * If a package name is supplied, we must attempt to add a new order first
  */
-if (property_exists($data, "package")) {
+try {
+	if (property_exists($data, "package")) {
 	/* form the query for getting the packageId */
-	$query = <<<"EOF_PACKAGE_QUERY"
-		SELECT packageId, available FROM packages
-		       WHERE UCASE(name) = UCASE("{$data->package}");
-EOF_PACKAGE_QUERY;
-
-	/* do the query */
-	if (($result = $mysqli->query($query)) === FALSE) {
-		$response["error"] = 'Query Error - ' . $mysqli->error;
-		goto database_quit;
-	}
-
-	/* store the new order if there was a package with this name */
-	if (($row = $result->fetch_assoc()) && 
-	    (!array_key_exists("available", $row) || $row["available"] > 0)) {
-		$packageId = $row["packageId"];
-
-		/*
-		 * let us see if the customer hasn't yet booked this trip
-		 */
-		$query = <<<"EOF_CHECK_QUERY"
-			SELECT packageId FROM orders
-			       WHERE customerId = $customerId
-			       AND packageId = $packageId;
-EOF_CHECK_QUERY;
+		$sth = $dbh->prepare(
+			"SELECT packageId, available FROM packages
+			        WHERE UCASE(name) = UCASE(:package)"
+		);
 		/* do the query */
-		if (($result = $mysqli->query($query)) === FALSE) {
-			$response["error"] = 'Query Error - ' . $mysqli->error;
+		$sth->execute(array(":package" => $data->package));
+
+		/* store the new order if there was a package with this name */
+		if (($row = $sth->fetch()) && 
+		    (!array_key_exists("available", $row) || $row["available"] > 0)) {
+			$packageId = $row["packageId"];
+
+			/*
+			 * let us see if the customer hasn't yet booked this trip
+			 */
+			$sth = $dbh->prepare(
+				"SELECT packageId FROM orders
+				       WHERE customerId = :customerId
+				       AND packageId = :packageId"
+			);
+			/* do the query */
+			$sth->execute(array(":customerId" => $customerId, ":packageId" => $packageId));
+
+			/* only proceed with the insert request if there is no record with this package name yet */
+			if (!$sth->fetch()) {
+				$sth = $dbh->prepare(
+					"INSERT INTO orders (customerId, packageId, status)
+					       VALUES (
+							:customerId, :packageId, 'Unpaid'
+					       )"
+				);
+				/* do the query */
+				$sth->execute(array(":customerId" => $customerId, ":packageId" => $packageId));
+			}
+		} // if ("available" > 0)
+	} // if (package name was supplied)
+
+
+	/*
+	 * Now retrieve all orders
+	 */
+	/* form the query for fetching the orders */
+	$sth = $dbh->prepare(	
+		"SELECT p.name AS package, i.fileName AS fileName, 
+			o.status, o.purchaseDate, o.receiptId
+			FROM orders o, packages p, images i
+			WHERE o.customerId = :customerId
+			AND p.packageId = o.packageId
+			AND i.imageName = p.imageName"
+	);
+        
+	/* do the query */
+	$sth->execute(array(":customerId" => $customerId));
+
+	/* fetch the results and put into response */
+	$response["data"] = array();
+	while ($row = $sth->fetch()) {
+		// append the row
+		$response["data"][] = $row;
+		
+		// check how many lines we have
+		if (count($response["data"]) > MAX_RESPONSE_LINES) {
+			$response["data"] = NULL;
+			$response["error"] = "response too large (over " . MAX_RESPONSE_LINES . " lines)";
 			goto database_quit;
 		}
-
-		/* only proceed with the insert request if there is no record with this package name yet */
-		if (!$result->fetch_assoc()) {
-			$query = <<<"EOF_INSERT_QUERY"
-				INSERT INTO orders (customerId, packageId, status)
-				       VALUES (
-						$customerId, $packageId, "Unpaid"
-				       );
-EOF_INSERT_QUERY;
-
-			/* do the query */
-			if ($mysqli->query($query) !== TRUE) {
-				$response["error"] = 'Query Error - ' . $mysqli->error;
-				goto database_quit;
-			}
-		}
 	}
-} // if (package name was supplied)
 
-
-/*
- * Now retrieve all orders
- */
-/* form the query for fetching the orders */
-$query = <<<"EOF_RETRIEVE_QUERY"
-	SELECT p.name AS package, i.fileName AS fileName, 
-	       o.status, o.purchaseDate, o.receiptId
-	       FROM orders o, packages p, images i
-	       WHERE o.customerId = $customerId
-	       AND p.packageId = o.packageId
-	       AND i.imageName = p.imageName;
-EOF_RETRIEVE_QUERY;
-        
-/* do the query */
-$response = array();
-if (($result = $mysqli->query($query)) === FALSE) {
-	$response["error"] = 'Query Error - ' . $mysqli->error;
+	/* put Merchant Id into the response */
+	if (count($response["data"])) {
+		require_once '../../../../comp199-www/paypal-credentials.php';
+		$response["merchant_id"] = PayPal_MerchantID;
+	}
+} catch (PDOException $e) {
+	$response["error"] = 'Query Error - ' . $e->getMessage();
 	goto database_quit;
-}
-
-/* fetch the results and put into response */
-$response["data"] = array();
-while ($row = $result->fetch_assoc()) {
-	// append the row
-	$response["data"][] = $row;
-	
-	// check how many lines we have
-	if (count($response["data"]) > MAX_RESPONSE_LINES) {
-		$response["data"] = NULL;
-		$response["error"] = "response too large (over " . MAX_RESPONSE_LINES . " lines)";
-		goto database_quit;
-	}
-}
-
-/* put Merchant Id into the response */
-if (count($response["data"])) {
-	require_once '../../../../comp199-www/paypal-credentials.php';
-	$response["merchant_id"] = PayPal_MerchantID;
 }
 
 database_quit:
 /* close the database */
-$mysqli->close();
+$dbh = null;
 
 quit:
 /* return the response */
@@ -153,7 +145,7 @@ return;
 
 auth_error_database:
 /* close the database */
-$mysqli->close();
+$dbh = null;
 
 auth_error:
 $response["error"] = "authentication";
