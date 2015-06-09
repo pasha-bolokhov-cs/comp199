ee17c200b677d853ffa534c3d670d1499d287cbb
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-06-09 10:41:41 -0700

    Load all description tables ahead of tracking the segment chain

--- php/packages.view.php
+++ php/packages.view.php
@@ -29,7 +29,7 @@ try {
 }
 
 try {
-	/* get the package description */
+	/** get the package description **/
 	$sth = $dbh->prepare(
 		"SELECT * FROM packages WHERE UCASE(name) = UCASE(:package) AND available > 0"
 	);
@@ -38,18 +38,97 @@ try {
 		$response["error"] = "package-sold-out";
 		goto database_quit;
 	}
+	/* check existence of all relevant fields */
+	foreach (array("packageId", "segId", "name", "region", "origin", "price",
+		       "description", "available", "imageName") as $field)
+		if (!array_key_exists($field, $package_row)) {
+			$response["error"] = "could not access package detail";
+			goto database_quit;
+		}
+
+	/* add package data to the response */
+	$response["package"] = $package_row;
+
+
+	/** load data from segments, locations, transport, flights, hotels and activities **/
+	/* load all from segments */
+	$segments = array();
+	foreach ($dbh->query("SELECT * FROM segments") as $row) {
+		foreach (array("segId", "location", "transportId", "flightId", "hotelId", "activityId",
+			       "duration", "nextSeg") as $field)
+			if (!array_key_exists($field, $row)) {
+				$response["error"] = "could not access package detail";
+				goto database_quit;
+			}
+		$segments[$row["segId"]] = $row;
+	}
+
+	/* load all from locations */
+	$locations = array();
+	foreach ($dbh->query("SELECT * FROM locations") as $row) {
+		foreach (array("city", "region", "country") as $field)
+			if (!array_key_exists($field, $row)) {
+				$response["error"] = "could not access package detail";
+				goto database_quit;
+			}
+		$locations[$row["city"]] = $row;
+	}
+
+	/* load all from transport */
+	$transport = array();
+	foreach ($dbh->query("SELECT * FROM transport") as $row) {
+		foreach (array("transportId", "type") as $field)
+			if (!array_key_exists($field, $row)) {
+				$response["error"] = "could not access package detail";
+				goto database_quit;
+			}
+		$transport[$row["transportId"]] = $row;
+	}
+
+	/* load all from flights */
+	$flights = array();
+	foreach ($dbh->query("SELECT * FROM flights") as $row) {
+		foreach (array("flightId", "flightNo", "origin", "departDate", "destination", "arriveDate") as $field)
+			if (!array_key_exists($field, $row)) {
+				$response["error"] = "could not access package detail";
+				goto database_quit;
+			}
+		$flights[$row["flightId"]] = $row;
+	}
+
+	/* load all from hotels */
+	$hotels = array();
+	foreach ($dbh->query("SELECT * FROM hotels") as $row) {
+		foreach (array("hotelId", "rank", "imageName", "description") as $field)
+			if (!array_key_exists($field, $row)) {
+				$response["error"] = "could not access package detail";
+				goto database_quit;
+			}
+		$hotels[$row["hotelId"]] = $row;
+	}
+
+	/* load all from activities */
+	$activities = array();
+	foreach ($dbh->query("SELECT * FROM activities") as $row) {
+		foreach (array("activityId", "name") as $field)
+			if (!array_key_exists($field, $row)) {
+				$response["error"] = "could not access package detail";
+				goto database_quit;
+			}
+		$activities[$row["activityId"]] = $row;
+	}
+
+	/** loop over segments **/
 	if (!array_key_exists("origin", $package_row)) {
 		$response["error"] = "could not access package detail";
 		goto database_quit;
 	}
-	$curr_location = row["origin"];
+	$curr_location = $row["origin"];
 	if (!array_key_exists("segId", $package_row)) {
 		$response["error"] = "could not access package detail";
 		goto database_quit;
 	}
 	$curr_seg = $package_row["segId"];
-
-	/* loop over segments */
 	$response["segments"] = array();
 	do {
 		$seg = array();
@@ -62,14 +141,26 @@ try {
 			$response["error"] = "could not access package detail";
 			goto database_quit;
 		}
-		/* if transport => create an extra row */
+		/* if transport => create an extra row for transport solely */
 		if (!array_key_exists("transportId", $row)) {
 			$response["error"] = "could not access package detail";
 			goto database_quit;
 		}
 		if ($row["transportId"]) {
+			$sth = $dbh->prepare(
+				"SELECT * FROM transport WHERE transport_id = :transport_id"
+			);
+			$sth->execute(array(":transport_id" => $row["transportId"]));
+			if (!($transport_row = $sth->fetch())) {
+				$response["error"] = "could not access package detail";
+				goto database_quit;
+			}
+			if (!array_key_exists("type", $transport_row)) {
+				$response["error"] = "could not access package detail";
+				goto database_quit;
+			}
+			$seg["transport"] = $transport_row["type"];
 
-			/* GGGG - implement */
 
 			$response["segments"][] = $seg;
 			$seg = array();
