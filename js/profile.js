b9d698a8983809cd72817026b65eac7d536f97be
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-06-12 22:30:47 -0700

    Remove extra spaces and correct style in "profile.js"

--- js/profile.js
+++ js/profile.js
@@ -16,7 +16,7 @@ app.controller('ProfileController', function($scope, $rootScope, $state, $http, 
 		$scope.customer = {};
 		$scope.customer.password = "";
 		$scope.customer.password2 = "";
-			
+
 		$scope.customer.email = $rootScope.storage.token.email;
 		$scope.customer.preemail = $scope.customer.email;
 		$scope.readOnly = true;
@@ -181,7 +181,7 @@ app.controller('ProfileController', function($scope, $rootScope, $state, $http, 
 	}
 	
 	$scope.passwordChange = function() {	
-        $scope.readOnly = true;
+		$scope.readOnly = true;
 		$scope.passwordStatus = true;
 		
 		$state.go("user.profile.password");	
@@ -189,7 +189,7 @@ app.controller('ProfileController', function($scope, $rootScope, $state, $http, 
 	debugger;
 	$scope.passwordConfirm = function() {
 		$scope.readOnly = true;
-		
+
 		// Indicate we are waiting for data
 		$rootScope.waiting = true;
         		
@@ -213,9 +213,9 @@ app.controller('ProfileController', function($scope, $rootScope, $state, $http, 
 				}
 				$state.go("user.profile.password");
 			} else {
-			    $scope.readOnly = true;
-		        $scope.passwordStatus = false;	
-			    $state.go("guest.home");		
+				$scope.readOnly = true;
+				$scope.passwordStatus = false;	
+				$state.go("guest.home");		
 			}
 		})
 		.error(function(data2, status) {		
