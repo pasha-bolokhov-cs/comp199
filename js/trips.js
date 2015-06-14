3ddead84796f8b183a999be57dcb81a511a22c5a
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-06-13 21:57:58 -0700

    Remove the unnecessary "sendEmail()" function

--- js/trips.js
+++ js/trips.js
@@ -132,8 +132,6 @@ console.log("GG EC token = ", $scope.ecToken);
 
 			// success - refresh the list of trips
 			$scope.getOrders();
-			// send confirmation email to customer
-			$scope.sendEmail();
 		})
 		.error(function(data, status) {
 			console.log(data);
@@ -143,18 +141,6 @@ console.log("GG EC token = ", $scope.ecToken);
 			$rootScope.waiting = false;
 		});			
 	};
-
-	/* send email function */
-	$scope.sendEmail = function(){/*
-        var conform = [$scope.customer.email, $scope.trips]	
-		$scope.conformEmail.email = "lelaywang@gmail.com";
-		$scope.conformEmail.merchantId = "hgeugbfvkfsiut3hr";
-		if ($scope.merchantId){
-			$http.post("php/secure/sendEmail.php", $scope.conformEmail);
-			}*/
-	};
-
-
 
 	/* order rejected or other failure */
 	$scope.rejected = function(url) {
