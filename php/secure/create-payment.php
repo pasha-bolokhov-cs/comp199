0e69c455802120d7b251b913b68913883775015d
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-06-20 13:51:07 -0700

    Convert 'create-payment.php' to PDO

--- php/secure/create-payment.php
+++ php/secure/create-payment.php
@@ -50,119 +50,121 @@ if (!validate($data->package)) {
 
 /* connect to the database */
 require_once '../../../../comp199-www/mysqli_auth.php';
-$mysqli = @new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
-if ($mysqli->connect_error) {
-	$response["error"] = 'Connect Error (' . $mysqli->connect_errno . ') '
-			     . $mysqli->connect_error;
+try {
+	$dbh = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB, MYSQL_USER, MYSQL_PASS);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	$response["error"] = 'Connect Error: ' . $e->getMessage();
 	goto quit;
 }
 
 /* get customerId */
-if (!($customerId = get_customerId($mysqli, $token))) {
+if (!($customerId = get_customerId_PDO($dbh, $token))) {
 	goto auth_error_database;
 }
 
 
-/* * * * * * * * * * * */
-/* * * * * * * * * * * */
-/*   Create  Payment   */
-/* * * * * * * * * * * */
-/* * * * * * * * * * * */
-
-/** Get Package Details **/
-/* form the query */
-$query = <<<"EOF_PACKAGE_QUERY"
-	SELECT name, price, available
-	       FROM packages
-	       WHERE UCASE(name) = UCASE("{$data->package}");
-EOF_PACKAGE_QUERY;
-
-/* do the query */
-if (($result = $mysqli->query($query)) === FALSE) {
-	$response["error"] = 'Query Error - ' . $mysqli->error;
-	goto database_quit;
-}
-if (!($row = $result->fetch_assoc())) {
-	$response["error"] = "package-non-existent";
-	goto database_quit;
-}
-if (array_key_exists("available", $row) && $row["available"] == 0) {
-	$response["error"] = "package-sold-out";
-	goto database_quit;
-}
-if (!array_key_exists("name", $row) || !array_key_exists("price", $row)) {
-	$response["error"] = "could not get package details";
-	goto database_quit;
-}
-$package = $row["name"];
-$price = $row["price"];
-
-
-/** Payer **/
-$payer = new Payer();
-$payer->setPaymentMethod("paypal");
-
-
-/** Payment Amount **/
-$amount = new Amount();
-$amount->setCurrency("CAD")
-	->setTotal($price);
-
-
-/** Transaction **/
-/* set package name as description */
-$transaction = new Transaction();
-$transaction->setAmount($amount)
-		->setDescription($package);
-
-
-/** Redirect urls after payment approval / cancellation **/
-$baseUrl = getBaseUrl();
-$redirectUrls = new RedirectUrls();
-$redirectUrls->setReturnUrl("$baseUrl/execute-payment.php?success=true")
-		->setCancelUrl("$baseUrl/execute-payment.php?success=false");
-
-
-/** Add payment details and set intent as 'sale' **/
-$payment = new Payment();
-$payment->setIntent("sale")
-	->setPayer($payer)
-	->setRedirectUrls($redirectUrls)
-	->setTransactions(array($transaction));
-
-
-/** Api Context **/
-$apiContext = new ApiContext(new OAuthTokenCredential(PayPal_App_ClientID, PayPal_App_Secret));
-
-/** Create Payment **/
 try {
-	$payment->create($apiContext);
-} catch (Exception $ex) {
-	$response["error"] = "exception: " . $ex->getMessage();
+
+	/* * * * * * * * * * * */
+	/* * * * * * * * * * * */
+	/*   Create  Payment   */
+	/* * * * * * * * * * * */
+	/* * * * * * * * * * * */
+
+	/** Get Package Details **/
+	$sth = $dbh->prepare(
+		"SELECT name, price, available
+			FROM packages
+			WHERE UCASE(name) = UCASE(:package)"
+	);
+	$sth->execute(array(":package" => $data->package));
+
+	if (!($row = $sth->fetch(PDO::FETCH_ASSOC))) {
+		$response["error"] = "package-non-existent";
+		goto database_quit;
+	}
+	if (array_key_exists("available", $row) && $row["available"] == 0) {
+		$response["error"] = "package-sold-out";
+		goto database_quit;
+	}
+	if (!array_key_exists("name", $row) || !array_key_exists("price", $row)) {
+		$response["error"] = "could not get package details";
+		goto database_quit;
+	}
+	$package = $row["name"];
+	$price = $row["price"];
+
+
+	/** Payer **/
+	$payer = new Payer();
+	$payer->setPaymentMethod("paypal");
+
+
+	/** Payment Amount **/
+	$amount = new Amount();
+	$amount->setCurrency("CAD")
+		->setTotal($price);
+
+
+	/** Transaction **/
+	/* set package name as description */
+	$transaction = new Transaction();
+	$transaction->setAmount($amount)
+			->setDescription($package);
+
+
+	/** Redirect urls after payment approval / cancellation **/
+	$baseUrl = getBaseUrl();
+	$redirectUrls = new RedirectUrls();
+	$redirectUrls->setReturnUrl("$baseUrl/execute-payment.php?success=true")
+			->setCancelUrl("$baseUrl/execute-payment.php?success=false");
+
+
+	/** Add payment details and set intent as 'sale' **/
+	$payment = new Payment();
+	$payment->setIntent("sale")
+		->setPayer($payer)
+		->setRedirectUrls($redirectUrls)
+		->setTransactions(array($transaction));
+
+
+	/** Api Context **/
+	$apiContext = new ApiContext(new OAuthTokenCredential(PayPal_App_ClientID, PayPal_App_Secret));
+
+	/** Create Payment **/
+	try {
+		$payment->create($apiContext);
+	} catch (Exception $ex) {
+		$response["error"] = "exception: " . $ex->getMessage();
+		goto database_quit;
+	}
+
+	/** Redirect buyer to paypal - in our case, extract the EC token **/
+	$approvalUrl = $payment->getApprovalLink();
+
+	$url_components = parse_url($approvalUrl);
+	if (!array_key_exists("query", $url_components)) {
+		$response["error"] = "approval URL has no parameters";
+		goto database_quit;
+	}
+	parse_str($url_components["query"], $url_params);
+	if (!array_key_exists("token", $url_params)) {
+		$response["error"] = "approval URL does not have a EC token";
+		goto database_quit;
+	}
+
+	/** Return the token to the client **/
+	$response["ec_token"] = $url_params["token"];
+
+} catch (PDOException $e) {
+	$response["error"] = 'Query Error - ' . $e->getMessage();
 	goto database_quit;
 }
-
-/** Redirect buyer to paypal - in our case, extract the EC token **/
-$approvalUrl = $payment->getApprovalLink();
-
-$url_components = parse_url($approvalUrl);
-if (!array_key_exists("query", $url_components)) {
-	$response["error"] = "approval URL has no parameters";
-	goto database_quit;
-}
-parse_str($url_components["query"], $url_params);
-if (!array_key_exists("token", $url_params)) {
-	$response["error"] = "approval URL does not have a EC token";
-	goto database_quit;
-}
-
-/** Return the token to the client **/
-$response["ec_token"] = $url_params["token"];
-
 
 database_quit:
 /* close the database */
-$mysqli->close();
+$dbh = null;
 
 quit:
 /* return the response */
@@ -174,7 +176,7 @@ return;
 
 auth_error_database:
 /* close the database */
-$mysqli->close();
+$dbh = null;
 
 auth_error:
 $response["error"] = "authentication";
