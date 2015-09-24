a1d73b0682464e82ee0ee7b9b605a1718c188896
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-09-24 09:35:39 -0700

    Add a descriptive comment to "auth-path.php"

--- php/auth-path.php
+++ php/auth-path.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Put the actual path to the directory where you store database and other authentication information.
+ * Note, that those files should be stored outside of the web-served area (such as "public_html").
+ * 
+ * Warning! Do NOT commit this file with private information put in place!
+ * If you need to edit and commit changes to this file, first anonymize any private values!
+ */
 define("AUTH_CONFIG_PATH", 'XYZXYZ-INSERT-YOUR-PATH-TO-DATABASE-AUTHENTICATION-INFORMATION-HERE-XYZXYZ');
 
 ?>
