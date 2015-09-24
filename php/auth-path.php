c08cef2ca910c21a85e994bc616f43e8fe9a1f8a
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-09-24 09:49:40 -0700

    Clarify the comment in "auth-path.php"

--- php/auth-path.php
+++ php/auth-path.php
@@ -2,6 +2,10 @@
 
 /*
  * Put the actual path to the directory where you store database and other authentication information.
+ * After you put the private information in, run
+ *     $ git update-index --assume-unchanged  auth-path.php
+ * to make Git ignore your local changes to this file
+ *
  * Note, that those files should be stored outside of the web-served area (such as "public_html").
  * 
  * Warning! Do NOT commit this file with private information put in place!
